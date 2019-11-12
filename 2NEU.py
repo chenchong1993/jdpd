76a7b685389d6b5155459e3a94a73caae5070738
chenchong1993 <43434953+chenchong1993@users.noreply.github.com>
2019-11-12 16:56:08 +0800

    python画图

--- 2NEU.py
+++ 2NEU.py
@@ -5,11 +5,14 @@ from mpl_toolkits.axisartist.axislines import SubplotZero
 import matplotlib.font_manager as fm
 myfont = fm.FontProperties(fname='C:/Windows/Fonts/simsun.ttc')
 
+plt.rcParams['font.sans-serif']=['simsun']
+plt.rcParams['axes.unicode_minus']=False
+
 import numpy as np
 def readFile(filename):
     f = open(filename,'r',encoding='utf-8') #如果文件不是uft-8编码方式，读取文件可能报错
     dataLines = f.readlines() #返回list，文件的每一行作为list的一个字符串元素
-    dataLines = dataLines[200:-250]  #读取行数的起始位置
+    # dataLines = dataLines[200:-250]  #读取行数的起始位置
     return dataLines
 
 
@@ -38,67 +41,106 @@ def getData(data = []):
         E.append(float(i.split()[0]))
         N.append(float(i.split()[1]))
         U.append(float(i.split()[2]))
+    print(E)
     return [E,N,U]
 
-# def draw(data = [],str = "x方向"):
-#     x = range(len(data))
-#     fig = plt.figure(1, (10, 6))
-#
-#     ax = SubplotZero(fig, 1, 1, 1)
-#     fig.add_subplot(ax)
-#     ax.axis["xzero"].set_visible(True)
-#     ax.axis["xzero"].label.set_color('green')
-#     plt.ylim(-5, 5)  # 限定纵轴的范围
-#     plt.plot(x, data)
-#     plt.legend()  # 让图例生效
-#     plt.margins(0)
-#     plt.subplots_adjust(bottom=0.15)
-#     plt.xlabel(u"eph") #X轴标签
-#     plt.ylabel("m") #Y轴标签
-#     plt.title(str) #标题
-#     plt.show()
 
-def draw(dataE = [],dataN = [],dataU = [],Me = "",Mn = "",Mu = ""):
-    eph = range(len(data))
-    fig = plt.figure()
-    # 将整个figure分成三行一列，第三个参数表示该图形放在第1个网格
-    ax = SubplotZero(fig, 3, 1, 1)
-    fig.add_subplot(ax)
-    ax.axis["xzero"].set_visible(True)
-    plt.ylim(-2, 2)  # 限定纵轴的范围
-    plt.scatter(eph, dataE,marker='x',s=0.5,color=(0.,0.5,0.))
-    plt.legend()  # 让图例生效
-    plt.margins(0)
-    #plt.xlabel(u"eph")  # X轴标签
-    #plt.ylabel("m")  # Y轴标签
-    plt.title("E方向残差/m",fontproperties=myfont)
-    plt.tight_layout()
-    plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
-                        wspace=0.5, hspace=0.5)
-    # 将整个figure分成三行一列，第三个参数表示该图形放在第2个网格
-    ax = SubplotZero(fig, 3, 1, 2)
-    fig.add_subplot(ax)
-    ax.axis["xzero"].set_visible(True)
-    plt.ylim(-2, 2)  # 限定纵轴的范围
-    plt.scatter(eph, dataN,marker='x',s=0.5,color=(0.,0.5,0.))
-    plt.legend()  # 让图例生效
-    plt.margins(0)
-    #plt.xlabel(u"eph")  # X轴标签
-    #plt.ylabel("m")  # Y轴标签
-    plt.title("N方向残差/m",fontproperties=myfont)
 
-    # 将整个figure分成三行一列，第三个参数表示该图形放在第3个网格
-    ax = SubplotZero(fig, 3, 1, 3)
-    fig.add_subplot(ax)
-    ax.axis["xzero"].set_visible(True)
-    plt.ylim(-3, 3)  # 限定纵轴的范围
-    plt.scatter(eph, dataU,marker='x',s=0.5,color=(0.,0.5,0.))
-    plt.legend()  # 让图例生效
-    plt.margins(0)
-    #plt.xlabel(u"eph")  # X轴标签
-    #plt.ylabel("m")  # Y轴标签
-    plt.title("U方向残差/m",fontproperties=myfont)
+def plot_2D(dataE = [],dataN = [],dataU = []):
 
+
+    e1 = dataE
+    n1 = dataN
+
+    std_e = np.std(e1)
+    std_n = np.std(n1)
+
+    var_e = 3.5  # 标准差
+    var_n = np.var(n1) ** 0.5  # 标准差
+
+    scale_y = max(var_e,var_n) # 刻度 四倍中误差
+
+    if scale_y > 200:  # 最大不超过2m
+        scale_y = 200
+
+    epoch = len(e1)
+
+    lim1 = [10] * epoch
+    lim2 = [-10] * epoch
+
+    x = np.linspace(0, epoch, epoch)  # 0到24 分240份
+
+    fig = plt.figure(1)
+    ax = SubplotZero(fig, 2, 1, 1)
+    fig.add_subplot(ax)
+
+    fig.set_size_inches(8, 4)
+    fig.subplots_adjust(left=0.13, bottom=0.2)  # 一些设置
+
+    # print(x)
+    # print(e1)
+
+    plt.plot(x, e1, "r.", label="E "+u"方向")
+    plt.plot(x, n1, "b.", label="N "+u"方向")
+
+    plt.plot(lim1, "m--")
+    plt.plot(lim2, "m--")
+
+
+    font2 = {'family': 'Times New Roman',
+             'weight': 'normal',
+             'size': 15,
+             }
+
+    plt.ylabel("残差/m", font2,fontproperties=myfont)
+    plt.xlabel("历元/s", font2,fontproperties=myfont)
+    # plt.title(format("平面残差"), font2,fontproperties=myfont)
+    plt.ylim(-scale_y, scale_y)
+    plt.legend()
+    plt.grid("on")
+    # plt.savefig("error of 2D" +"("")"+ "{0}{1}.png".format(" ", name), dpi=300)
+# ---------------------------------------U方向---------------------------------------------------------------------------
+    u1 = dataU
+
+    std_u = np.std(u1)
+
+    var_u = 5 # 标准差
+    var_n = np.var(u1) ** 0.5  # 标准差
+
+    scale_y = max(var_u, var_n)  # 刻度 四倍中误差
+
+    if scale_y > 200:  # 最大不超过2m
+        scale_y = 200
+
+    epoch = len(u1)
+    #   print("历元数为=", epoch)
+
+    lim1 = [10] * epoch
+    lim2 = [-10] * epoch
+
+    x = np.linspace(0, epoch, epoch)  # 0到24 分240份
+
+    ax = SubplotZero(fig, 2, 1, 2)
+    fig.add_subplot(ax)
+
+    fig.set_size_inches(8, 4)
+    fig.subplots_adjust(left=0.13, bottom=0.2)  # 一些设置
+    #
+    # print(x)
+    # print(u1)
+
+    plt.plot(x, u1, "g.", label="U " + u"方向")
+
+    plt.plot(lim1, "m--")
+    plt.plot(lim2, "m--")
+
+
+    plt.ylabel("残差/m", font2, fontproperties=myfont)
+    plt.xlabel("历元/s", font2, fontproperties=myfont)
+    # plt.title(format("U方向残差"), font2,fontproperties=myfont)
+    plt.ylim(-scale_y, scale_y)
+    plt.legend()
+    plt.grid("on")
     plt.show()
 
 
@@ -110,7 +152,7 @@ def getM(data = []):
     dataM = sqrt(dataM2)
     return dataM
 
-filename = 'C:\\Users\\chenc\\Desktop\\不规则格网\\不同高程测试\\最终数据\\方案2\\222NEU.txt'
+filename = 'E:\\湖南测试\\11.11\\第三次测试\\MI8差分 (1).txt'
 data = readFile(filename)
 
 ENU = getData(data)
@@ -118,18 +160,20 @@ ENU = getData(data)
 E=ENU[0]
 N=ENU[1]
 U=ENU[2]
-
+print("1111111111111111111111111111111111111111")
 print(E)
 print(N)
 print(U)
-
-Me = getM(E)
-Mn = getM(N)
-Mu = getM(U)
-print(Me,Mn,Mu,sqrt(Me*Me+Mn*Mn),sqrt(Me*Me+Mn*Mn+Mu*Mu))
+print("1111111111111111111111111111111111111111")
+# Me = getM(E)
+# Mn = getM(N)
+# Mu = getM(U)
+# print(Me,Mn,Mu,sqrt(Me*Me+Mn*Mn),sqrt(Me*Me+Mn*Mn+Mu*Mu))
 '''
 Me = Me[0:8]
 Mn = Mn[0:8]
 Mu = Mu[0:8]
 '''
-draw(E,N,U,Me,Mn,Mu)
+# draw(E,N,U,Me,Mn,Mu)
+plot_2D(E,N,U)
+
