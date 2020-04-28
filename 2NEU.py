46fc9d1621e3dda5a09a83f65d5e6c256cfc0b3c
chenchong1993 <43434953+chenchong1993@users.noreply.github.com>
2020-04-28 18:21:44 +0800

    修改作图

--- 2NEU.py
+++ 2NEU.py
@@ -143,6 +143,73 @@ def plot_2D(dataE = [],dataN = [],dataU = []):
     plt.grid("on")
     plt.show()
 
+def plot_enu(data1 = [],data2 = [],data3 = []):
+
+# 修改配置----------------------------------------------
+# 第一张图配置:
+    data1_scale_y = 15 # y轴范围
+    data1_epoch = len(data1) #x轴范围
+    data1_yname = "errors/m"
+    data1_xname = "epoch/s"
+    data1_label = "E"
+# 第一张图配置:
+    data2_scale_y = 8 # y轴范围
+    data2_epoch = len(data2) #x轴范围
+    data2_yname = "errors/m"
+    data2_xname = "epoch/s"
+    data2_label = "N"
+# 第一张图配置:
+    data3_scale_y = 30 # y轴范围
+    data3_epoch = len(data3) #x轴范围
+    data3_yname = "errors/m"
+    data3_xname = "epoch/s"
+    data3_label = "U"
+
+# 绘制第一张图----------------------------------------------------
+    E = np.linspace(0, data1_epoch, data1_epoch)  # 0到24 分240份
+    N = np.linspace(0, data2_epoch, data2_epoch)  # 0到24 分240份
+    fig = plt.figure(1)
+    ax = SubplotZero(fig, 2, 1, 1)
+    ax.axhline(0, color='black', lw=1)
+    fig.add_subplot(ax)
+    fig.set_size_inches(8, 4)
+    fig.subplots_adjust(hspace=0.5)
+    plt.plot(E, data1, "g.", label=data1_label)
+    plt.plot(N, data2, "r.", label=data2_label)
+    plt.ylabel(data1_yname, fontproperties=myfont)
+    plt.xlabel(data1_xname, fontproperties=myfont)
+    plt.ylim(-data1_scale_y, data1_scale_y)
+    plt.legend()
+    # plt.grid("on")
+
+# 绘制二张图----------------------------------------------------
+    U = np.linspace(0, data3_epoch, data3_epoch)  # 0到24 分240份
+    fig = plt.figure(1)
+    ax = SubplotZero(fig, 2, 1, 2)
+    ax.axhline(0, color='black', lw=1)
+    fig.add_subplot(ax)
+    fig.set_size_inches(8, 4)
+    plt.plot(U, data3, "g.", label=data3_label)
+    plt.ylabel(data3_yname, fontproperties=myfont)
+    plt.xlabel(data3_xname, fontproperties=myfont)
+    plt.ylim(-data3_scale_y, data3_scale_y)
+    plt.legend()
+    # plt.grid("on")
+# 绘制三张图----------------------------------------------------
+#     x = np.linspace(0, data3_epoch, data3_epoch)  # 0到24 分240份
+#     fig = plt.figure(1)
+#     ax = SubplotZero(fig, 3, 1, 3)
+#     ax.axhline(0, color='black', lw=1)
+#     fig.add_subplot(ax)
+#     fig.set_size_inches(8, 4)
+#     plt.plot(x, data3, "g.", label=data3_label)
+#     plt.ylabel(data3_yname, fontproperties=myfont)
+#     plt.xlabel(data3_xname, fontproperties=myfont)
+#     plt.ylim(-data3_scale_y, data3_scale_y)
+#     plt.legend()
+    # plt.grid("on")
+# ------------------------------------------------------------------------------------------------
+    plt.show()
 
 def getM(data = []):
     dataSum = 0
@@ -152,8 +219,8 @@ def getM(data = []):
     dataM = sqrt(dataM2)
     return dataM
 
-# filename = 'E:\\湖南测试\\论文数据\\虚拟值\\201911131806TestVRSpos.txt'
-filename = 'E:\\湖南测试\\论文数据\\改正数\\201911131806TestGRIDpos.txt'
+filename = 'G:\\湖南测试\\论文数据\\单点\\201910211155TestSPPpos.txt'
+# filename = 'E:\\湖南测试\\论文数据\\改正数\\201911131806TestGRIDpos.txt'
 data = readFile(filename)
 
 ENU = getData(data)
@@ -166,15 +233,16 @@ print(E)
 print(N)
 print(U)
 print("1111111111111111111111111111111111111111")
-# Me = getM(E)
-# Mn = getM(N)
-# Mu = getM(U)
-# print(Me,Mn,Mu,sqrt(Me*Me+Mn*Mn),sqrt(Me*Me+Mn*Mn+Mu*Mu))
+Me = getM(E)
+Mn = getM(N)
+Mu = getM(U)
+print(Me,Mn,Mu,sqrt(Me*Me+Mn*Mn),sqrt(Me*Me+Mn*Mn+Mu*Mu))
 '''
 Me = Me[0:8]
 Mn = Mn[0:8]
 Mu = Mu[0:8]
 '''
 # draw(E,N,U,Me,Mn,Mu)
-plot_2D(E,N,U)
+plot_enu(E,N,U)
+
 
--- multpathPlot.py
+++ multpathPlot.py
@@ -14,6 +14,8 @@ def readFile(filename):
     for i in dataLines:
         if "C" in i:
             pass
+        elif "E" in i:
+            pass
         elif "G" in i:
             pass
         elif "0 -1" in i:
@@ -53,34 +55,34 @@ def getSTD(data = []):
     std = sqrt(std2)
     return std
 
-def plot_2D(data1 = [],data2 = [],data3 = [],data4=[]):
+def plot_2D(data1 = [],dataName1='',data2 = [],dataName2='',data3 = [],dataName3='',data4=[],dataName4=''):
 
 # 修改配置----------------------------------------------
-# 第一张图配置:
+# 第一个数据配置:
     data1_scale_y = 8 # y轴范围
     data1_epoch = len(data1) #x轴范围
     data1_yname = "multipath errors/m"
     data1_xname = "epoch/s"
-    data1_label = "Huawei P30 L1"
-# 第一张图配置:
+    data1_label = dataName1
+# 第二个数据配置:
     data2_scale_y = 8 # y轴范围
     data2_epoch = len(data2) #x轴范围
     data2_yname = "multipath errors/m"
     data2_xname = "epoch/s"
-    data2_label = "xiaomi M8 L1"
-# 第一张图配置:
+    data2_label = dataName2
+# 第三个数据配置:
     data3_scale_y = 1 # y轴范围
     data3_epoch = len(data3) #x轴范围
     data3_yname = "multipath errors/m"
     data3_xname = "epoch/s"
-    data3_label = "Huawei P30 L5"
-# 第一张图配置:
+    data3_label = dataName3
+# 第四个数据配置:
     data4_scale_y = 1 # y轴范围
     data4_epoch = len(data4) #x轴范围
     data3_yname = "multipath errors/m"
     data3_xname = "epoch/s"
-    data4_label = "xiaomi M8 L5"
-# 绘制第一张图----------------------------------------------------
+    data4_label = dataName4
+# 绘制第一张图-data1,data3---------------------------------------------------
     x3 = np.linspace(0, data3_epoch, data3_epoch)  # 0到24 分240份
     x = np.linspace(0, data1_epoch, data1_epoch)  # 0到24 分240份
     fig = plt.figure(1)
@@ -89,7 +91,7 @@ def plot_2D(data1 = [],data2 = [],data3 = [],data4=[]):
     fig.add_subplot(ax)
     fig.set_size_inches(8, 4)
     fig.subplots_adjust(hspace=0.5)
-    plt.plot(x, data1, "g.", label=data1_label)
+    plt.plot(x, data1, "b.", label=data1_label)
     plt.plot(x3, data3, "r.", label=data3_label)
     plt.ylabel(data1_yname, fontproperties=myfont)
     plt.xlabel(data1_xname, fontproperties=myfont)
@@ -97,7 +99,7 @@ def plot_2D(data1 = [],data2 = [],data3 = [],data4=[]):
     plt.legend()
     # plt.grid("on")
 
-# 绘制二张图----------------------------------------------------
+# 绘制二张图--data2-data4-------------------------------------------------
     x4 = np.linspace(0, data4_epoch, data4_epoch)  # 0到24 分240份
     x = np.linspace(0, data2_epoch, data2_epoch)  # 0到24 分240份
     fig = plt.figure(1)
@@ -128,34 +130,37 @@ def plot_2D(data1 = [],data2 = [],data3 = [],data4=[]):
 # ------------------------------------------------------------------------------------------------
     plt.show()
 
-fileR9 = 'gpsr9.m12'
-dataR9 = readFile(fileR9)
 
-fileP30L1 = 'gpsp30.m15'
-dataP30L1 = readFile(fileP30L1)
-fileP30L5 = 'gpsp30.m51'
-dataP30L5 = readFile(fileP30L5)
+# data1和data3在第一张图上；data2和data4在第二张图上
+file1 = 'P30_E.m15'
+data1 = readFile(file1)
+data1 = data1[0:6000]
+dataName1 = 'Huawei P30 E1'
 
-fileM8L1 = 'gpsm8.m15'
-dataM8L1 = readFile(fileM8L1)
-fileM8L5 = 'gpsm8.m51'
-dataM8L5 = readFile(fileM8L5)
+file2 = 'P30_E.m51'
+data2 = readFile(file2)
+data2 = data2[0:6000]
+dataName2 = 'Huawei P30 E5a'
 
-multPathR9 = getData(dataR9)
-multPathP30L1 = getData(dataP30L1)
-multPathM8L1 = getData(dataM8L1)
-multPathP30L5 = getData(dataP30L5)
-multPathM8L5 = getData(dataM8L5)
-# print(getSTD(multPathP30L1))
-# print(getSTD(multPathP30L5))
+file3 = 'P30-GPS.m15'
+data3 = readFile(file3)
+data3 = data3[0:6000]
+dataName3 = 'Huawei P30 L1'
 
-print(getSTD(multPathR9))
-print(getSTD(multPathM8L1))
-print(getSTD(multPathP30L1))
-print(getSTD(multPathM8L5))
-print(getSTD(multPathP30L5))
-# print(np.std(multPathP30L1))
-# print(np.std(multPathM8))
-# print(np.std(multPathR9))
-# plot_2D(multPathP30L1,multPathM8L1,multPathP30L5,multPathM8L5)
-# plot()
+file4 = 'P30-GPS.m51'
+data4 = readFile(file4)
+data4 = data4[0:6000]
+dataName4 = 'Huawei P30 L5'
+
+multPathData1 = getData(data1)
+multPathData2 = getData(data2)
+multPathData3 = getData(data3)
+multPathData4 = getData(data4)
+
+print(dataName1,getSTD(multPathData1))
+print(dataName2,getSTD(multPathData2))
+print(dataName3,getSTD(multPathData3))
+print(dataName4,getSTD(multPathData4))
+
+plot_2D(multPathData1,dataName1,multPathData2,dataName2,multPathData3,dataName3,multPathData4,dataName4)
+plot()
